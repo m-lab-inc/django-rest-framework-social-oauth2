839ee5a444eecd9295ccfa16c1add7c933742e82
hamaike <yusuke.hamaike@mlabs.jp>
2025-02-05 12:04:55 +0900

    django5.0 対応

--- rest_framework_social_oauth2/backends.py
+++ rest_framework_social_oauth2/backends.py
@@ -5,11 +5,12 @@ except ImportError:  # Will be removed in Django 2.0
 
 from social_core.backends.oauth import BaseOAuth2
 from .settings import DRFSO2_PROPRIETARY_BACKEND_NAME, DRFSO2_URL_NAMESPACE
+from .urls import app_name
 
 class DjangoOAuth2(BaseOAuth2):
     """Default OAuth2 authentication backend used by this package"""
     name = DRFSO2_PROPRIETARY_BACKEND_NAME
     AUTHORIZATION_URL = reverse(DRFSO2_URL_NAMESPACE + ':authorize'
-                                if DRFSO2_URL_NAMESPACE else 'authorize')
+                                if DRFSO2_URL_NAMESPACE else f'{app_name}:authorize')
     ACCESS_TOKEN_URL = reverse(DRFSO2_URL_NAMESPACE + ':token'
-                               if DRFSO2_URL_NAMESPACE else 'token')
+                               if DRFSO2_URL_NAMESPACE else f'{app_name}:token')
